5d5f60516f6c187b778a2ad7f55b79f0ac716e4e
AndresNovoa88 <andresnovoa334@gmail.com>
2022-08-19 15:51:35 -0500

    v3-Completamos Productos

--- src/productos.java
+++ src/productos.java
@@ -1,4 +1,15 @@
 
 public class productos {
     
+    private String nombre;
+    private int precio;
+    private String categoria;
+
+    public productos(String nombre, int precio, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+    
+    
 }
