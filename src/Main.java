a3924d3f5382f2fae4187b547d27a3230cce263d
AndresNovoa88 <andresnovoa334@gmail.com>
2022-08-19 15:31:54 -0500

    v2-Agregamos Productos

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -10,11 +5,11 @@
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Provando Repositorio");
+        System.out.println("Provando Repositorio segunda version");
+        
     }
     
 }
